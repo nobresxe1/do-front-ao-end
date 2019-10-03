ab1872ba4037d92eee3cf72c955e281b2ce33b49
Rodrigo Garcia <rgs190385@gmail.com>
2019-10-03 00:09:05 -0300

    Arrumando a busca e corrigido alguns break points

---
+++ js/input-search.js
@@ -0,0 +1,7 @@
+const $inputSearch = document.querySelector(".input-search");
+const $icon = $inputSearch.querySelector(".icon");
+
+$icon.addEventListener("click", event => {
+    event.preventDefault();
+    $inputSearch.classList.toggle("-active")
+})
